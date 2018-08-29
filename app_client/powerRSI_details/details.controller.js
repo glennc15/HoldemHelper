14bb2c9e42c610f8d66e9db97c8d9668dd2975fa
Glenn <gcrosby15@gmail.com>
2018-08-29 13:24:40 +0800

    Add method to build daily OHLC chart. Modify details.controller to use the chart. Updated the view to display the chart

--- app_client/powerRSI_details/details.controller.js
+++ app_client/powerRSI_details/details.controller.js
@@ -4,8 +4,8 @@
     .module('loc8rApp')
     .controller('detailsCtrl', detailsCtrl);
 
-    detailsCtrl.$inject = ['$routeParams', '$interval', 'powerRSI_Data'];
-    function detailsCtrl ($routeParams, $interval, powerRSI_Data) {
+    detailsCtrl.$inject = ['$routeParams', '$interval', 'powerRSI_Data', 'detailComponents'];
+    function detailsCtrl ($routeParams, $interval, powerRSI_Data, detailComponents) {
       var vm = this;
 
       powerRSI_Data.details_data($routeParams.symbolid)
@@ -20,104 +20,269 @@
             spreads: data.spreads
           };
 
-          chart = new Highcharts.Chart({
-            chart: {
-                renderTo: 'win_loss_chart',
-                type: 'pie',
-                height: 250
-            },
-            title: {
-                text: 'Winning Trades vs. Losing Trades'
-            },
-            // yAxis: {
-            //     title: {
-            //         text: 'Total percent market share'
-            //     }
-            // },
-            plotOptions: {
-                pie: {
-                    shadow: false,
-                    // colors: ["#90ed7d", "#f15c80"]
-                    colors: ["#90ed7d", "#f45b5b"]
-                    // ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", 
-                    //  "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
-                }
-            },
-            tooltip: {
-                formatter: function() {
-                  var tooltip_str = '<b>Total Trades</b>: ' + data['backtest_data']['total_trades'] + '<br>';
-                  tooltip_str = tooltip_str + '<b>'+ this.point.name +'</b>: '+ this.y +'<br>';
-                  tooltip_str = tooltip_str + '<b>Winning Percent</b>: ' + data['backtest_data']['winning_pct'] + '%<br>';
-                  tooltip_str = tooltip_str + '<b>Average Days In Trade</b>: ' + data['backtest_data']['mean_trade_days'];
+          var histTradesSummaryChart = detailComponents.histTradesSummaryChart(
+                                          total_trades=data['backtest_data']['total_trades'],
+                                          winning_pct=data['backtest_data']['winning_pct'],
+                                          mean_trade_days=data['backtest_data']['mean_trade_days'],
+                                          hist_trades_sumamry_data=data['backtest_data']['trades_data']);
+
+          var histReturnsSummaryChart = detailComponents.histReturnsSummaryChart(data['backtest_data']['returns_data']);
+          
+          
 
 
-                  // return '<b>'+ this.point.name +'</b>: '+ this.y +'';
-                  return tooltip_str;
-                }
-            },
-            series: [{
-                name: 'Browsers',
-                data: data['backtest_data']['trades_data'],
-                // data: [["Winning Trades",89],["Loosing Trades",4]],
-                // size: '50%',
-                size: '95%',
-                innerSize: '90%',
-                showInLegend:true,
-                dataLabels: { enabled: false }
-            }]
-          });
 
-          Highcharts.chart('mean_gains_chart', {
-            chart: {
-                type: 'column',
-                inverted: true,
-                height: 250
-            },
-            title: {
-                text: 'Average Returns'
-            },
-            // subtitle: {
-            //     text: 'Observed in Vik i Sogn, Norway, 2017'
-            // },
-            xAxis: {
-                categories: ['Avg Winning Return', 'Avg Return', 'Avg Losing Return']
-            },
-            yAxis: {
-                title: {
-                    text: 'Average % Return'
-                }
-            },
-            tooltip: {
-                valueSuffix: '%'
-            },
-            colors: ["#90ed7d",  "#7cb5ec" ,"#f45b5b"],
-            //     // ["#7cb5ec", "#434348", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", 
-            //     //  "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"]
-            plotOptions: {
-              column: {
-                colorByPoint: true
-              },
-              columnrange: {
-                  dataLabels: {
-                      enabled: true,
-                      format: '{y}%'
-                  }
-              },
-            },
-            legend: {
-                enabled: false
-            },
-            series: [{
-                // name: 'Return',
-                data: data['backtest_data']['returns_data'],
-                // data: [
-                //     [1.74],
-                //     [1.58],
-                //     [-2.04]
-                // ]
-            }]
 
-          });          
+          // **********
+
+          // $.getJSON('https://www.highcharts.com/samples/data/aapl-ohlc.json', function (data) {
+
+          // ****
+
+          // var ohlc_data = data['daily_data']['ohlc_series'];
+          // var volume_data = data['daily_data']['volume_series'];
+
+
+        
+
+          // var groupingUnits = [[
+          //     'week',                         // unit name
+          //     [1]                             // allowed multiples
+          //   ], 
+          //   [
+          //     'month',
+          //     [1, 2, 3, 4, 6]
+          //   ]
+          // ];
+
+          // var chart_symbol = $routeParams.symbolid;
+          // ***
+              // create the chart
+
+          var daily_ohlc_chart = detailComponents.dailyOHLCChart(chart_symbol=$routeParams.symbolid, ohlc_data=data['daily_data']['ohlc_series'], volume_data=data['daily_data']['volume_series']);
+
+          // Highcharts.stockChart('daily_ohlc_chart', 
+          //   {
+
+          //     rangeSelector: 
+          //     {
+          //       selected: 1
+          //     },
+
+          //     title: 
+          //     {
+          //       text: chart_symbol + " Daily"
+          //     },
+
+          //     chart: {
+          //       height: "50%"
+          //     },
+
+          //     yAxis: 
+          //     [
+          //       {
+          //         labels: 
+          //         {
+          //           align: 'right',
+          //           x: -3
+          //         },
+          //         title: 
+          //         {
+          //           text: 'OHLC'
+          //         },
+          //         height: '60%',
+          //         lineWidth: 2,
+          //         resize: 
+          //         {
+          //           enabled: true
+          //         }
+          //       }, 
+          //       {
+          //         labels: 
+          //         {
+          //           align: 'right',
+          //           x: -3
+          //         },
+          //         title: 
+          //         {
+          //           text: 'Volume'
+          //         },
+          //         top: '65%',
+          //         height: '30%',
+          //         offset: 0,
+          //         lineWidth: 2
+          //       },
+          //       {
+          //         labels: 
+          //         {
+          //           align: 'right',
+          //           x: -3
+          //         },
+          //         title: 
+          //         {
+          //           text: 'RSI'
+          //         },
+          //         top: '100%',
+          //         height: '20%',
+          //         offset: 0,
+          //         lineWidth: 2
+          //       }
+          //     ],
+
+          //     tooltip: 
+          //     {
+          //       split: true
+          //     },
+
+          //     series: 
+          //     [
+          //       {
+          //         type: 'candlestick',
+          //         name: chart_symbol,
+          //         id: 'aapl',
+          //         yAxis: 0,
+          //         // zIndex: 2,
+          //         data: ohlc_data,
+          //         dataGrouping: 
+          //         {
+          //             units: groupingUnits
+          //         }
+          //       },
+          //       {
+          //         type: 'sma',
+          //         linkedTo: 'aapl',
+          //         // zIndex: 1,
+          //         tooltip: 
+          //         {
+          //           valueDecimals: 2
+          //         },
+          //         marker: 
+          //         {
+          //           enabled: false
+          //         },
+          //         params: 
+          //         {
+          //           period: 200
+          //         }
+          //       }, 
+          //       {
+          //         type: 'column',
+          //         name: 'Volume',
+          //         // id: 'volume-series',
+          //         data: volume_data,
+          //         yAxis: 1,
+          //         // dataGrouping: {
+          //         //     units: groupingUnits
+          //         // }
+          //       }, 
+
+          //       {
+          //         type: 'rsi',
+          //         linkedTo: 'aapl',
+          //         // zIndex: 1,
+          //         yAxis: 2,
+          //         marker: {
+          //             enabled: false
+          //         },
+          //         tooltip: {
+          //           valueDecimals: 2
+          //         },
+          //         params: {
+          //           decimals: 4,
+          //           period: 4
+          //         }
+          //       }
+          //     ]
+          //   });
+
+
+// series: [{
+//   id: ‘main-series’,
+//   data: [ … ]
+// }, {
+//   id: ‘volume-series’,
+//   yAxis: 1,
+//   data: [ … ]
+// }, {
+//   type: ‘mfi’,
+//   linkedTo: ‘main-series’,
+//   yAxis: 2,
+//   params: {
+//     volumeSeriesID: ‘volume-series’
+//   }
+// }]
+
+            // Highcharts.stockChart('container', {
+            //   rangeSelector: {
+            //       selected: 2
+            //   },
+            //   yAxis: [
+            //     {
+            //       height: '75%',
+            //       resize: {
+            //           enabled: true
+            //       },
+            //       labels: {
+            //           align: 'right',
+            //           x: -3
+            //       },
+            //       title: {
+            //           text: 'AAPL'
+            //       }
+            //       }, 
+            //       {
+            //         top: '75%',
+            //         height: '25%',
+            //         labels: {
+            //             align: 'right',
+            //             x: -3
+            //         },
+            //         offset: 0,
+            //         title: {
+            //             text: 'MACD'
+            //         }
+            //       }],
+            //       title: {
+            //           text: 'AAPL Stock Price'
+            //       },
+            //       subtitle: {
+            //           text: 'With MACD and Pivot Points technical indicators'
+            //       },
+
+            //       series: [
+            //         {
+            //           type: 'ohlc',
+            //           id: 'aapl',
+            //           name: 'AAPL Stock Price',
+            //           data: ohlc_data,
+            //           zIndex: 1
+            //         },
+            //         {
+            //           type: 'pivotpoints',
+            //           linkedTo: 'aapl',
+            //           zIndex: 0,
+            //           lineWidth: 1,
+            //           dataLabels: {
+            //               overflow: 'none',
+            //               crop: false,
+            //               y: 4,
+            //               style: {
+            //                   fontSize: 9
+            //               }
+            //             }
+            //         }, 
+            //         {
+            //           type: 'macd',
+            //           yAxis: 1,
+            //           linkedTo: 'aapl'
+            //         }]
+            // });
+
+
+        // });
+
+
+          // ********** 
 
 
         })
